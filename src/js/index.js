3d8c0b5f3b13c2726d273bdf3994a429460a29ff
Andy Alexis <andy_mar_rey@outlook.com>
2020-08-06 12:00:08 -0500

    SE SUBIO AL SERVIDOR AVANCE

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,33 @@
-new Glide(".glide").mount();
-new Glide(".dona-empresas__slider").mount();
+let banner = document.querySelector(".glide");
+if (banner) {
+  new Glide(".glide").mount();
+}
 
-var glide = new Glide("#intro", {
-  type: "carousel",
-  perView: 6,
-});
+let empresas = document.getElementById("intro");
+if (empresas) {
+  var glide = new Glide(empresas, {
+    type: "carousel",
+    perView: 6,
+  });
+  glide.mount();
+}
+let catalogo = document.getElementById("catalogo");
+if (catalogo) {
+  var glideCatalogo = new Glide(catalogo, {
+    type: "carousel",
+    perView: 3,
+  });
 
-var glideCatalogo = new Glide("#catalogo", {
-  type: "carousel",
-  perView: 3,
+  glideCatalogo.mount();
+}
+$("#btn-animales").click(function () {
+  $.ajax({
+    url: "pag-animales.html",
+    data: "data",
+    success: function (data) {
+      setTimeout(function () {
+        $("#contenido-ajax").html(data);
+      });
+    },
+  });
 });
-glide.mount();
---
+++ src/js/modal-youtube.js
@@ -0,0 +1,4 @@
+let clase = document.querySelector(".card-title__video");
+if (clase) {
+  $(".card-title__video").modalVideo();
+}
