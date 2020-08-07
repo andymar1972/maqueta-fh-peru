8ba1432d878e5b5ee96300a9dd38c2e84e228b1b
Andy Alexis <andy_mar_rey@outlook.com>
2020-08-06 20:26:08 -0500

    se agrego cambios ajax

--- src/js/index.js
+++ src/js/index.js
@@ -38,3 +38,25 @@ $("#btn-animales").click(function () {
     },
   });
 });
+$("#btn-educacion").click(function () {
+  $.ajax({
+    url: "pag-educacion.html",
+    data: "data",
+    success: function (data) {
+      setTimeout(function () {
+        $("#contenido-ajax").html(data);
+      });
+    },
+  });
+});
+$("#btn-salud").click(function () {
+  $.ajax({
+    url: "pag-salud.html",
+    data: "data",
+    success: function (data) {
+      setTimeout(function () {
+        $("#contenido-ajax").html(data);
+      });
+    },
+  });
+});
