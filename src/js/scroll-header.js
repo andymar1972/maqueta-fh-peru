7f16a280d1a8de1053a0a659af95624b9cfc0cee
Andy Mariños <andy_mar_rey@outlook.com>
2020-08-26 10:33:42 -0500

    arreglo header top fixed

--- src/js/scroll-header.js
+++ src/js/scroll-header.js
@@ -1,13 +1,13 @@
 const header = document.getElementById("header");
 window.onscroll = function () {
   var scroll = document.documentElement.scrollTop || document.body.scrollTop;
-  if (scroll > 25) {
+  if (scroll > 62) {
     header.style.position = "fixed";
     header.style.top = "0";
     header.style.width = "100%";
     header.style.zIndex = "15";
   }
-  if (scroll < 25) {
+  if (scroll < 62) {
     header.style.position = "relative";
     header.style.top = "0";
     header.style.width = "100%";
